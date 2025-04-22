606d9be4dda568d89370744c1766aa85cf89512a
Nixos <nirgranthshah@gmail.com>
2025-04-22 19:11:44 +0530

    remove db

--- backend/contollers/productController.js
+++ backend/contollers/productController.js
@@ -1,14 +1,15 @@
 export const getProducts = async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Products fetched successfully',
-  });
-}
-
-export const getProductById = async (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({
-    success: true,
-    message: `Product with id ${id} fetched successfully`,
-  });
-}
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully',
+    });
+  }
+  
+  export const getProductById = async (req, res) => {
+    const { id } = req.params;
+    res.status(200).json({
+      success: true,
+      message: `Product with id ${id} fetched successfully`,
+    });
+  }
+  
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 import { sql_queries } from './sql_queries.js';
 import { pgClient } from './postgres_db.js';
 import productRoutes from './routes/productRoutes.js';
+import e from 'express';
 
 dotenv.config();
 
@@ -22,25 +23,30 @@ app.use(express.json()); // for parsing application/json
 
 app.use('/api/products', productRoutes); // for product routes
 
+app.use(express.static(path.join(__dirname,"/frontend/build"))); // for serving static files
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend","build","index.html")); // for serving index.html for all other routes
+});
 
 async function connectDB() {
     try {
         await pgClient.connect();
         await pgClient.query(sql_queries.createProductTableQuery);
         console.log('Connected to the database');
-        return true;
+        // return true;
     } catch (err) {
         console.error('Error connecting to the database', err.stack);
-        return false;
+        // return false;
     }
 }
 
 // Modified server startup
 connectDB().then((connected) => {
-    if (!connected) {
-        console.error('Server startup failed: Could not connect to database');
-        process.exit(1); // Exit with error code
-    }
+    // if (!connected) {
+    //     console.error('Server startup failed: Could not connect to database');
+    //     process.exit(1); // Exit with error code
+    // }
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
