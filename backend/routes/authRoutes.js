547452d55a3ad553d173faf8d2bb07681398ffb5
Nixos <nirgranthshah@gmail.com>
2025-04-30 18:43:15 +0530

    fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import * as productController from '../contollers/productController.js';
 import * as authController from '../contollers/authController.js';
 
 const router = express.Router();
 
-router.get('/login/', authController.login); // for logging in
-router.get('/logout/', authController.logout); // for logging out
-router.get('/register/', authController.register); // for user registration
+router.post('/login/', authController.login); // for logging in
+router.post('/logout/', authController.logout); // for logging out
+router.post('/register/', authController.register); // for user registration
 
 export default router;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getProducts, getProductById } from '../contollers/productController.js';
+import * as productController from '../contollers/productController.js';
 
 const router = express.Router();
 
-router.get('/', getProducts); // for getting all products
-router.get('/:id', getProductById); // for getting a product by id
+router.get('/', productController.getProducts); // for getting all products
+router.get('/:id', productController.getProductById); // for getting a product by id
 
 
 export default router;
